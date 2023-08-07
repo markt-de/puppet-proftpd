897a16e4ac862185dec1f0286ab1a5c91b2a784e
Frank Wall <fw@moov.de>
2023-08-07 17:54:58 +0200

    fix acceptance tests on Rocky/RHEL 8

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,10 @@
 class proftpd::install {
   assert_private()
 
+  if $facts['os']['family'] == 'RedHat' {
+     Yumrepo <| |> -> Package <| |>
+  }
+
   if $proftpd::package_manage {
     package { $proftpd::packages:
       ensure => $proftpd::package_ensure,
